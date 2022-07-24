d8ab8baa7a1bcdd25ece8b3cf68e921a0bb78d41
Arth I Jani <85545030+ArthIJani@users.noreply.github.com>
2022-07-24 10:09:57 +0530

    Add files via upload

---
+++ Day-15/CodingExercise01.py
@@ -0,0 +1,9 @@
+from turtle import Turtle, Screen
+timmy = Turtle()
+timmy.shape("turtle")
+timmy.color("coral")
+timmy.forward(100)
+
+my_screen = Screen()
+print(my_screen.canvheight)
+my_screen.exitonclick()
