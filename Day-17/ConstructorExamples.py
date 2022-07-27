ab5c3acca4a3ee24d790f983672e996fd7e51d3a
Arth I Jani <85545030+ArthIJani@users.noreply.github.com>
2022-07-27 12:10:19 +0530

    Add files via upload

---
+++ Day-17/ConstructorExamples.py
@@ -0,0 +1,21 @@
+#Constructors
+class User:
+  def __init__(self, user_id, username):
+    self.id = user_id
+    self.username = username
+    self.followers = 0
+    self.following = 0
+
+  def follow(self, user):
+    user.followers+= 1
+    self.following += 1
+
+user_1 = User("001", "Arth9547")
+user_2 = User("002", "AJCodes22")
+user_3 = User("003", "AJBlogsProgramming")
+user_4 = User("004", "AJQuotes22")
+
+user_1.follow(user_2)
+
+print(user_1.followers)
+print(user_2.followers)
